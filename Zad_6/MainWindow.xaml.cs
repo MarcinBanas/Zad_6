1ca1940ba391b35165c7e923c4bd63164bb09447
MarcinBanas <mb056625@student.ath.edu.pl>
2022-07-25 19:32:59 +0200

    Dodaj pliki projektów.

---
+++ Zad_6/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Markup;
+
+namespace Zad_6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var path = @"C:\Users\admin\source\repos\Zad_6\Zad_6\Dictionary1.xaml";
+            using var stream = new FileStream(path, FileMode.Open);
+            var content = XamlReader.Load(stream);
+            this.Resources.MergedDictionaries.Add((ResourceDictionary)content);
+        }
+    }
+}
